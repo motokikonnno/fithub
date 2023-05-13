c0604c8067ec383445ab872baf21cf3dc624b0d1
motoki <motoki610972@icloud.com>
2023-05-13 21:59:22 +0900

    feat-パンくずリスト作成

---
+++ src/components/BreadCrumb.tsx
@@ -0,0 +1,33 @@
+import { FC, memo } from "react";
+import styles from "../styles/components/BreadCrumb.module.scss";
+import { itemType } from "./pages/MyProfile";
+
+type BreadCrumbProps = {
+  folderTitle?: itemType[];
+  repository: string;
+  handleViewRepository: (id: string) => void;
+};
+
+export const BreadCrumb: FC<BreadCrumbProps> = memo(
+  ({ folderTitle, repository, handleViewRepository }) => {
+    return (
+      <div className={styles.container}>
+        <div className={styles.title} onClick={() => handleViewRepository("")}>
+          {repository}
+        </div>
+        {folderTitle &&
+          folderTitle.map((title) => (
+            <div key={title.id} className={styles.titleContainer}>
+              <span className={styles.slash}>/</span>
+              <div
+                className={styles.title}
+                onClick={() => handleViewRepository(title.id)}
+              >
+                {title.name}
+              </div>
+            </div>
+          ))}
+      </div>
+    );
+  }
+);
