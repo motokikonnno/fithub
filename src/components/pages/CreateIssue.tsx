c8a31dbef84f42c888060a24a6ca3795e62b9093
motoki <motoki610972@icloud.com>
2023-05-14 21:15:25 +0900

    feat-createIssueページ作成

---
+++ src/components/pages/CreateIssue.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "../../styles/components/pages/CreateIssue.module.scss";
+
+export const CreateIssue = React.memo(() => {
+  return <div></div>;
+});
---
+++ src/pages/[user_name]/[repository_name]/issue/new.tsx
@@ -0,0 +1,8 @@
+import { CreateIssue } from "@/components/pages/CreateIssue";
+import { NextPage } from "next";
+
+const CreateIssuePage: NextPage = () => {
+  return <CreateIssue />;
+};
+
+export default CreateIssuePage;
