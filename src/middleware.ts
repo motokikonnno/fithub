10fa5118dd383337a58e71b762256af0c0cb71a5
motoki <motoki610972@icloud.com>
2023-05-30 19:33:48 +0900

    feat-middleware追加

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  secret: process.env.SECRET,
+});
+
+export const config = {
+  matcher: ["/((?!sign_in|[^/.]).*)"],
+};
