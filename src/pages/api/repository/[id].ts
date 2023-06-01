506ba1471f7157cb48d75cae9894e779f232ed4a
motoki <motoki610972@icloud.com>
2023-06-02 00:11:25 +0900

    fix-apiRoutesのディレクトリ変更

--- src/pages/api/repository/[id].ts
+++ src/pages/api/repository/[id].ts
@@ -1,17 +1,19 @@
-import { getRepository } from "@/lib/api/repository";
+import { createRepository, getRepository } from "@/lib/api/repository";
 import { HttpMethod } from "@/types/http";
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 
-const handleRepositoryRequest: NextApiHandler = async (
+const handleRepositoryAllRequest: NextApiHandler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
   switch (req.method) {
     case HttpMethod.GET:
       return getRepository(req, res);
+    case HttpMethod.POST:
+      return createRepository(req, res);
     default:
       return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 };
 
-export default handleRepositoryRequest;
+export default handleRepositoryAllRequest;
--- src/pages/api/repository/index.ts
+++ src/pages/api/repository/index.ts
@@ -1,19 +1,17 @@
-import { createRepository, getRepositories } from "@/lib/api/repository";
+import { getRepositories } from "@/lib/api/repository";
 import { HttpMethod } from "@/types/http";
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 
-const handleRepositoryAllRequest: NextApiHandler = async (
+const handleRepositoriesRequest: NextApiHandler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
   switch (req.method) {
     case HttpMethod.GET:
-      return getRepositories(req, res);
-    case HttpMethod.POST:
-      return createRepository(req, res);
+      return await getRepositories(req, res);
     default:
       return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 };
 
-export default handleRepositoryAllRequest;
+export default handleRepositoriesRequest;
