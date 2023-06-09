c2928b7f4ceb71e087f5fe284312898823d21c48
motoki <motoki610972@icloud.com>
2023-06-10 00:10:48 +0900

    feat-saidbarにデータ表示

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -2,18 +2,20 @@ import { SideBar } from "./SideBar";
 import styles from "../../styles/components/layouts/AppLayout.module.scss";
 import { Header } from "./Header";
 import React from "react";
+import { UserBelongsToTeam } from "@/models/User";
 
 type AppLayoutProps = {
   children: React.ReactNode;
+  user: UserBelongsToTeam;
 };
 
 export const AppLayout: React.FC<AppLayoutProps> = React.memo(
-  ({ children }) => {
+  ({ children, user }) => {
     return (
       <>
         <Header />
         <div className={styles.mainContents}>
-          <SideBar />
+          <SideBar user={user} />
           <div className={styles.layoutRight}>{children}</div>
         </div>
       </>
--- src/components/layouts/SideBar.tsx
+++ src/components/layouts/SideBar.tsx
@@ -1,53 +1,118 @@
-import { mockRepositories } from "@/mock/mockRepositories";
-import { mockTeams } from "@/mock/mockTeams";
+import { UserBelongsToTeam } from "@/models/User";
+import { shapeOwnerDataWithRepositories } from "@/services/ownerList";
+import { recentSortRepositories } from "@/services/recentSortRepositories";
+import { OwnerWithRepositories } from "@/types/owner";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { FC, useEffect, useMemo, useRef, useState } from "react";
 import styles from "../../styles/components/layouts/SideBar.module.scss";
 import { InputSearch } from "../InputSearch";
-import { ownerList, ownerType } from "../pages/CreateRepository";
 
-export const SideBar = React.memo(() => {
+type SideBarProps = {
+  user: UserBelongsToTeam;
+};
+
+export const SideBar: FC<SideBarProps> = React.memo(({ user }) => {
+  const ownerList: {
+    owners: OwnerWithRepositories[];
+    ownerData: OwnerWithRepositories;
+  } = useMemo(() => {
+    const userData = shapeOwnerDataWithRepositories(user).userData;
+    const teamList = shapeOwnerDataWithRepositories(user).teamData;
+    return { owners: [userData, ...teamList], ownerData: userData };
+  }, [user]);
   const [toggleOwnerList, setToggleOwnerList] = useState(false);
-  const [owner, setOwner] = useState({ name: "motoki", icon: "/logo.png" });
+  const [owner, setOwner] = useState<OwnerWithRepositories>(
+    ownerList.ownerData
+  );
+  const [type, setType] = useState<"user" | "team">("user");
+  const sortRepositories =
+    owner.repositories && recentSortRepositories(owner.repositories);
+  const recentRepositories = sortRepositories?.slice(0, 7);
+  const [currentRepositories, setCurrentRepositories] =
+    useState(recentRepositories);
+  const [currentOwnerList, setCurrentOwnerList] = useState(ownerList.owners);
+  const defaultImage =
+    "https://firebasestorage.googleapis.com/v0/b/fithub-a295f.appspot.com/o/default%2Fif2dmi1ea10tfgha.png?alt=media&token=6b1fa117-48f3-4858-9383-7b86e70685b0";
+  const dropDownListRef = useRef<HTMLDivElement>(null);
 
-  const handleSetOwner = (owner: ownerType) => {
+  useEffect(() => {
+    const ownerArray: OwnerWithRepositories[] = ownerList.owners.filter(
+      ({ id }) => id !== owner.id
+    );
+    setCurrentOwnerList(ownerArray);
+  }, [owner.id, ownerList]);
+
+  useEffect(() => {
+    const handleClickToCloseDropDown = (event: MouseEvent) => {
+      if (!(event.target instanceof HTMLElement)) {
+        return;
+      }
+      const element = dropDownListRef.current;
+
+      if (element && element?.contains(event.target)) return;
+      setToggleOwnerList(false);
+    };
+    window.addEventListener("click", handleClickToCloseDropDown, true);
+    return () => {
+      window.removeEventListener("click", handleClickToCloseDropDown);
+    };
+  }, []);
+
+  const handleSetOwner = (owner: OwnerWithRepositories) => {
     setOwner(owner);
     setToggleOwnerList(false);
+    setType(owner.type);
+    setCurrentRepositories(owner.repositories);
   };
   return (
     <div className={styles.container}>
-      <div className={styles.profileNameWrapper}>
+      <div className={styles.profileNameWrapper} ref={dropDownListRef}>
         <div
           className={styles.contentWrap}
           onClick={() => setToggleOwnerList(!toggleOwnerList)}
         >
-          <Image
-            src={"/logo.png"}
-            width={20}
-            height={20}
-            alt="profile画像"
-            className={styles.avatar}
-          />
+          {owner.image ? (
+            <Image
+              src={owner.image}
+              width={20}
+              height={20}
+              alt="profile画像"
+              className={styles.avatar}
+            />
+          ) : (
+            <Image
+              src={defaultImage}
+              width={20}
+              height={20}
+              alt="profile画像"
+              className={styles.avatar}
+            />
+          )}
+
           <span className={styles.profileName}>{owner.name}</span>
         </div>
         {toggleOwnerList && (
           <ul className={styles.listWrapper}>
-            {ownerList.map((owner, index) => (
-              <li
-                onClick={() => handleSetOwner(owner)}
-                key={index}
-                className={styles.listItem}
-              >
-                <Image
-                  src={owner.icon}
-                  width={16}
-                  height={16}
-                  alt="owner-icon"
-                />
-                {owner.name}
-              </li>
-            ))}
+            {currentOwnerList.map(
+              (owner, index) =>
+                owner.image && (
+                  <li
+                    onClick={() => handleSetOwner(owner)}
+                    key={index}
+                    className={styles.listItem}
+                  >
+                    <Image
+                      src={owner.image}
+                      width={16}
+                      height={16}
+                      alt="owner-icon"
+                      className={styles.avatar}
+                    />
+                    {owner.name}
+                  </li>
+                )
+            )}
           </ul>
         )}
       </div>
@@ -75,24 +140,42 @@ export const SideBar = React.memo(() => {
             borderColor={"#d0d7de"}
           />
         </div>
-        {mockRepositories.map((mockRepository, index) => (
-          <div key={index} className={styles.itemList}>
-            <div className={styles.avatarWrapper}>
-              <Image
-                src={mockRepository.image}
-                width={20}
-                height={20}
-                alt="profile画像"
-                className={styles.avatar}
-              />
+        {currentRepositories &&
+          currentRepositories.map((repository, index) => (
+            <div key={index} className={styles.itemList}>
+              <div className={styles.avatarWrapper}>
+                {owner.image ? (
+                  <Image
+                    src={owner.image}
+                    width={20}
+                    height={20}
+                    alt="profile画像"
+                    className={styles.avatar}
+                  />
+                ) : (
+                  <Image
+                    src={defaultImage}
+                    width={20}
+                    height={20}
+                    alt="profile画像"
+                    className={styles.avatar}
+                  />
+                )}
+              </div>
+              <div className={styles.breakWord}>
+                <Link
+                  className={styles.itemName}
+                  href={
+                    type === "user"
+                      ? `/user/${owner.id}/repository/${repository.id}`
+                      : `/team/${owner.id}/repository/${repository.id}`
+                  }
+                >
+                  {repository.name}
+                </Link>
+              </div>
             </div>
-            <div className={styles.breakWord}>
-              <Link className={styles.itemName} href="/dashboard">
-                {mockRepository.name}
-              </Link>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
       <div>
         <h2 className={styles.sectionTitle}>Your teams</h2>
@@ -104,18 +187,21 @@ export const SideBar = React.memo(() => {
             borderColor={"#d0d7de"}
           />
         </div>
-        {mockTeams.map((mockTeam, index) => (
-          <div key={index} className={styles.itemList}>
-            <Image
-              src={mockTeam.image}
-              width={20}
-              height={20}
-              alt="profile画像"
-              className={styles.avatar}
-            />
-            <span className={styles.itemName}>{mockTeam.name}</span>
-          </div>
-        ))}
+        {user.team_members &&
+          user.team_members.map(({ team }, index) => (
+            <div key={index} className={styles.itemList}>
+              <Image
+                src={team.image}
+                width={20}
+                height={20}
+                alt="profile画像"
+                className={styles.avatar}
+              />
+              <Link href={`/team/${team.id}`} className={styles.itemName}>
+                {team.name}
+              </Link>
+            </div>
+          ))}
       </div>
     </div>
   );
