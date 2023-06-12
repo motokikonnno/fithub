efd56720c0c9da2766ff8bd229965df106c5ac1b
motoki <motoki610972@icloud.com>
2023-06-12 09:20:56 +0900

    fix-Loading画面に変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
+import { Loading } from "@/components/Loading";
 import { Dashboard } from "@/components/pages/Dashboard";
 import useFetchUser from "@/hooks/useFetchUser";
 import { AuthNextPage } from "@/types/auth-next-page";
 import { useSession } from "next-auth/react";
-import ErrorPage from "./404";
 
 const DashboardPage: AuthNextPage = () => {
   const { data: session } = useSession();
@@ -10,7 +10,7 @@ const DashboardPage: AuthNextPage = () => {
   if (user) {
     return <Dashboard user={user} />;
   } else {
-    return <ErrorPage />;
+    return <Loading />;
   }
 };
 
