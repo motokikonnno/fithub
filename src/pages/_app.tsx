21fddda0ba52e3acea23e4a8d37377ad7babdc19
motoki <motoki610972@icloud.com>
2023-06-25 23:00:22 +0900

    feat-ページ遷移間にローディング表示

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,9 @@ import { SessionProvider } from "next-auth/react";
 import { AuthGuard } from "@/components/layouts/AuthGuard";
 import { usePageView } from "@/hooks/usePageView";
 import { Progressbar } from "@/components/Progressbar";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Loading } from "@/components/Loading";
 
 export type AuthAppProps = AppProps<{ session: Session }> & {
   Component: NextComponentType & { requireAuth?: boolean };
@@ -15,10 +18,29 @@ export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AuthAppProps) {
-  usePageView();
+  // usePageView();
+  const router = useRouter();
+  const [pageLoading, setPageLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = (url: string) =>
+      url !== router.asPath && setPageLoading(true);
+    const handleComplete = () => setPageLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  });
   return (
     <SessionProvider session={session}>
-      <Progressbar />
+      {/* <Progressbar /> */}
+      {pageLoading && <Loading />}
       {Component.requireAuth ? (
         <AuthGuard>
           <Component {...pageProps} />
