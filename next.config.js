7248927f7be5c5f58582384547015b4f9980dc5e
motoki <motoki610972@icloud.com>
2023-09-18 22:43:00 +0900

    feat-セキュリティ対策

--- next.config.js
+++ next.config.js
@@ -10,6 +10,43 @@ const nextConfig = {
     ],
   },
   swcMinify: false,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "sameorigin",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=()",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self' 'unsafe-inline' www.googletagmanager.com",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
